9b5dabc9ed19edc64b10613cac9d14d44cf54457
Franz Enzenhofer <franz.enzenhofer@fullstackoptimization.com>
2025-10-01 14:02:24 +0200

    feat(build): update filter-builder build order with EVI-native 15-section structure

--- build.js
+++ build.js
@@ -35,18 +35,22 @@ buildHTML('query-builder.html', [
   'query-builder/99-foot.html'
 ]);
 
-// Build filter-builder.html (cloned from query-builder structure)
+// Build filter-builder.html (EVI.gv.at native structure)
 buildHTML('filter-builder.html', [
   'filter-builder/00-head.html',
   'filter-builder/01-basics.html',
-  'filter-builder/02-organizations.html',
-  'filter-builder/03-locations.html',
-  'filter-builder/04-headline.html',
-  'filter-builder/05-content.html',
-  'filter-builder/05b-seo.html',
-  'filter-builder/05c-interlinking.html',
-  'filter-builder/06-qc-publish.html',
-  'filter-builder/07-matrix-preview.html',
+  'filter-builder/02-families.html',
+  'filter-builder/03-aussendungstypen.html',
+  'filter-builder/04-organizations.html',
+  'filter-builder/05-locations.html',
+  'filter-builder/06-extra-filters.html',
+  'filter-builder/07-sort-quality.html',
+  'filter-builder/08-headline.html',
+  'filter-builder/09-content.html',
+  'filter-builder/10-seo.html',
+  'filter-builder/11-interlinking.html',
+  'filter-builder/12-matrix-preview.html',
+  'filter-builder/13-qc-publish.html',
   'filter-builder/99-foot.html'
 ]);
 
